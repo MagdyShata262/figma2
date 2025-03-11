09300af3cf7078b4d3e6ebc281c4d3281b4f0603
magdy shata <magdyshata35@gmail.com>
2025-03-11 09:52:14 +0200

    add-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoComponent } from './video/video.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
